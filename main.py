7807cf068b101dff411afc8f3fae26c01caaa058
Shane B. <dotfortun@users.noreply.github.com>
2026-07-29 15:18:50 +0000

    Added scalar docs.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
+
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference()
+
+
+@app.get("/hello")
+async def hello_world():
+    return {
+        "messge": "Hello world",
+    }
